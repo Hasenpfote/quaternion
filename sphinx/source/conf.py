da446ba5502ac50f3f958977a0d244668ea6317e
Hasenpfote <Hasenpfote@gmail.com>
2018-05-13 11:54:57 +0900

    Change settings

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -31,8 +31,20 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.mathjax']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.mathjax'
+]
+
+autodoc_default_flags = [
+    'members',
+    #'undoc-members',
+    #'private-members',
+    'special-members',
+    #'inherited-members',
+    #'show-inheritance'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
